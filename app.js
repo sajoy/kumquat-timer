4a68fb0610fb3912bd6e1f4ac4c6484a17812983
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-06-21 11:39:51 -0700

    only accept valid time, add to dom on creation

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ Timer.prototype.save = function () {
 }
 
 Timer.prototype.start = function () {
+    
+    document.getElementById( 'modal' ).classList.add( 'show' );
     console.log( this.name , ' is starting!' );
 
     this.clock.innerHTML = '<span id="minutes">' 
@@ -70,6 +72,24 @@ Timer.prototype.stop = function () {
     clearInterval( this.currentTime );
 }
 
+Timer.prototype.addToDom = function () {
+    var timerLi = document.createElement( 'li' );
+
+    var nameHeader = document.createElement( 'h2' );
+    nameHeader.classList.add( 'clock' );
+    nameHeader.innerText = twoPlaces( this.minutes ) + ':' + twoPlaces( this.seconds );
+
+    var startBtn = document.createElement( 'button' );
+    startBtn.setAttribute( 'type', 'submit' );
+    startBtn.setAttribute( 'title', 'start timer' );
+    startBtn.innerHTML = randEmoji();
+    startBtn.addEventListener( 'click', this.start.bind( this ) );
+
+    timerLi.appendChild( nameHeader );
+    timerLi.appendChild( startBtn );
+    timersUl.appendChild( timerLi );
+}
+
 function randomInt (min, max) {
     return Math.floor( Math.random() * (max - min + 1) ) + min;
 }
@@ -84,52 +104,52 @@ function randEmoji () {
     return emojis[randomInt(0,2)];
 }
 
+function isValidTime ( minutes, seconds ) {
+    var minNum = parseInt( minutes ); 
+    var secNum = parseInt( seconds );
+
+    var overZero = minNum + secNum !== 0;
+    var underSixty = secNum < 60;
+
+    return overZero && underSixty;
+}
 
 
 
 var timersUl = document.getElementById( 'timers' );
 
 if ( localStorage.timers ) {
+    document.getElementById( 'empty-state' ).classList.remove( 'show' );
+
     var timers = JSON.parse( localStorage.timers );
     timers.forEach( function ( timer ) {
         var timerObj = new Timer( timer.name, timer.minutes, timer.seconds )
         Timer.all.push( timerObj );
-
-        var timerLi = document.createElement( 'li' );
-
-        var nameHeader = document.createElement( 'h2' );
-        nameHeader.classList.add( 'clock' );
-        nameHeader.innerText = twoPlaces( timer.minutes ) + ':' + twoPlaces( timer.seconds );
-
-        var startBtn = document.createElement( 'button' );
-        startBtn.setAttribute( 'type', 'submit' );
-        startBtn.setAttribute( 'title', 'start timer' );
-        startBtn.innerHTML = randEmoji();
-
-        startBtn.addEventListener( 'click', function () {
-            timerObj.start();
-            document.getElementById( 'modal' ).classList.add( 'show' );
-        });
-
-        timerLi.appendChild( nameHeader );
-        timerLi.appendChild( startBtn );
-        timersUl.appendChild( timerLi );
+        timerObj.addToDom();
     });
 }
 
 var form = document.getElementsByTagName( 'form' )[0];
 form.addEventListener( 'submit', function ( event ) {
     event.preventDefault();
-    var minutes = document.getElementById( 'minutes' ).value;
-    var seconds = document.getElementById( 'seconds' ).value;
+    var minInput = document.getElementById( 'minutes' );
+    var secInput = document.getElementById( 'seconds' );
+    var minutes = minInput.value;
+    var seconds = secInput.value;
     
-    if ( parseInt( minutes ) + parseInt( seconds ) === 0 ) { 
-        alert( 'We need more time!' );
+    if ( !isValidTime( minutes, seconds ) ) { 
+        alert( 'Try again, buster.' );
         return;
     }
 
     var timer = new Timer( name, minutes, seconds );
     timer.save();
+    timer.addToDom();
+
+    minInput.value = 'XX';
+    secInput.value = 'XX';
+    secInput.blur();
+    minInput.blur();
 });
 
 var modalCloseBtn = document.getElementById( 'close-modal' );
